36223d43c38bbdd0b29e8ae5bff692ffef0901ae
Prachi <prachikale136@gmail.com>
2023-06-23 22:46:52 +0530

    [Prachi] Add: create linked list and insert the data

---
+++ DataStructureLinkedList/DataStructureLinkedList/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    internal class LinkedList
+    {
+        public Node Head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.Head == null)
+            {
+                this.Head = node;
+            }
+            else
+            {
+                Node temp = Head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.Data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.Head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Data + " ");
+                temp = temp.Next;
+            }
+        }
+    }
+}
---
+++ DataStructureLinkedList/DataStructureLinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
